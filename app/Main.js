ac869f9fcd9e4cd4875e5f6cd50469a09b8c79eb
fateme <f.adiban.rad@gmail.com>
2024-10-26 10:20:34 +0330

    new

--- app/Main.js
+++ app/Main.js
@@ -2,11 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 function Main() {
-  return (
-    <div>
-      <h1>This is our app!!!</h1>
-    </div>
-  )
+  return <div></div>
 }
 
 const root = ReactDOM.createRoot(document.querySelector("#app"))
