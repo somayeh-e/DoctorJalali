ad7086a6b13deb2b2eeca39a5a9d152f4ac63ca3
somayeh <smyh.e003@gmail.com>
2024-10-27 00:11:30 +0330

    فارسی کردن قسمت ها

--- app/Main.js
+++ app/Main.js
@@ -6,6 +6,7 @@ function Main() {
     <>
       <header className="header-bar bg-primary mb-3">
         <div className="container d-flex flex-column flex-md-row align-items-center p-3">
+          <img src={"images/QomUni.png"} />
           <h4 className="my-0 mr-md-auto font-weight-normal">
             <a href="/" className="text-white">
               {" "}
@@ -15,13 +16,13 @@ function Main() {
           <form className="mb-0 pt-2 pt-md-0">
             <div className="row align-items-center">
               <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-                <input name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autocomplete="off" />
+                <input name="username" className="form-control form-control-sm input-dark" type="text" placeholder="نام کاربری" autocomplete="off" />
               </div>
               <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-                <input name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
+                <input name="password" className="form-control form-control-sm input-dark" type="password" placeholder="رمز عبور" />
               </div>
               <div className="col-md-auto">
-                <button className="btn btn-success btn-sm">Sign In</button>
+                <button className="btn btn-success btn-sm">ورود دانشجو</button>
               </div>
             </div>
           </form>
@@ -31,31 +32,31 @@ function Main() {
       <div className="container py-md-5">
         <div className="row align-items-center">
           <div className="col-lg-7 py-3 py-md-5">
-            <h1 className="display-3">Remember Writing?</h1>
-            <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
+            <h1 className="display-3">برنامه ریزی هوشمند؟</h1>
+            <p className="lead text-muted">سایت برنامه هفتگی اساتید به شما امکان می‌دهد تا به‌راحتی به زمان‌بندی کلاس‌ها و برنامه‌های آموزشی اساتید دسترسی پیدا کنید و با اطلاعات به‌روز، برنامه‌ریزی بهتری داشته باشید</p>
           </div>
           <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
             <form>
               <div className="form-group">
                 <label for="username-register" className="text-muted mb-1">
-                  <small>Username</small>
+                  <small>نام کاربری</small>
                 </label>
-                <input id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autocomplete="off" />
+                <input id="username-register" name="username" className="form-control" type="text" placeholder="یک نام کاربری انتخاب کنید" autocomplete="off" />
               </div>
               <div className="form-group">
                 <label for="email-register" className="text-muted mb-1">
-                  <small>Email</small>
+                  <small>ایمیل</small>
                 </label>
                 <input id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autocomplete="off" />
               </div>
               <div className="form-group">
                 <label for="password-register" className="text-muted mb-1">
-                  <small>Password</small>
+                  <small>رمز عبور</small>
                 </label>
-                <input id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
+                <input id="password-register" name="password" className="form-control" type="password" placeholder="یک رمز عبور ایجاد کنید" />
               </div>
               <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
-                Sign up for ComplexApp
+                ثبت نام دانشجو
               </button>
             </form>
           </div>
@@ -65,11 +66,11 @@ function Main() {
       <footer className="border-top text-center small text-muted py-3">
         <p>
           <a href="/" className="mx-1">
-            Home
+            خانه
           </a>{" "}
           |{" "}
           <a className="mx-1" href="/about-us">
-            About Us
+            درباره ما
           </a>{" "}
           |{" "}
           <a className="mx-1" href="/terms">
