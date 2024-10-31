d5e3ff9f6a786e40dc15ccedc20f22ed02d1b819
fateme <f.adiban.rad@gmail.com>
2024-10-31 18:44:37 +0330

    new

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -8,29 +8,22 @@ function HomeGuest() {
         <div className="row align-items-center">
           <div className="col-lg-7 py-3 py-md-5 direction">
             <h1 className="display-3">برنامه ریزی هوشمند؟</h1>
-            <p className="lead text-muted">سایت برنامه هفتگی اساتید به شما امکان می‌دهد تا به‌راحتی به زمان‌بندی کلاس‌ها و برنامه‌های آموزشی اساتید دسترسی پیدا کنید و با اطلاعات به‌روز، برنامه‌ریزی بهتری داشته باشید</p>
+            <p className="lead text-muted">سایت برنامه هفتگی اساتید به شما امکان می‌دهد تا به‌راحتی به زمان‌بندی کلاس‌ها و برنامه‌های آموزشی اساتید دسترسی پیدا کنید و با اطلاعات به‌روز، برنامه‌ریزی بهتری داشته باشید.</p>
           </div>
           <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5 direction">
             <form>
               <div className="form-group">
-                <label for="email-register" className="text-muted mb-1">
-                  <small>عکس پروفایل</small>
-                </label>
-                <input id="email-register" name="email" className="form-control" type="file" autocomplete="off" />
-              </div>
-
-              <div className="form-group">
                 <label for="username-register" className="text-muted mb-1">
-                  <small>نام و نام خانوادگی</small>
+                  <small>شماره دانشجویی</small>
                 </label>
-                <input id="username-register" name="username" className="form-control" type="text" placeholder="یک نام کاربری انتخاب کنید" autocomplete="off" />
+                <input id="username-register" name="username" className="form-control" type="text" placeholder="شماره دانشجویی خود را وارد کنید" autocomplete="off" />
               </div>
 
               <div className="form-group">
                 <label for="password-register" className="text-muted mb-1">
                   <small>رمز عبور</small>
                 </label>
-                <input id="password-register" name="password" className="form-control" type="password" placeholder="یک رمز عبور ایجاد کنید" />
+                <input id="password-register" name="password" className="form-control" type="password" placeholder="رمز عبور خود را وارد کنید" />
               </div>
               <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
                 ثبت نام دانشجو
